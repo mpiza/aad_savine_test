c79f88121a2798f0d3353b9da7fee746212ba880
Marcelo Piza <send2marcelo@gmail.com>
2021-11-13 10:42:48 -0500

    latest changes

--- aad_full_test/aad_full_test.cpp
+++ aad_full_test/aad_full_test.cpp
@@ -2,19 +2,115 @@
 //
 
 #include <iostream>
+#include "aad.h"
+#include "black_scholes.h"
+
+
+ // A simple Multiplier class
+template < class T>
+struct Multiplier
+{
+	const T multiplyBy;
+	Multiplier(const double m) : multiplyBy(m) {}
+	T operator () (const T y) const
+{
+		return multiplyBy * y;
+	}
+};
+
+template <class T>
+T f(T x[5])
+{
+	T y1 = x[2] * (5.0 * x[0] + x[1]);
+	T y2 = log(y1);
+	T y = (y1 + x[3] * y2) * (y1 + y2);
+	return y;
+};
+
+template <class T>
+T f2(T x[5])
+{
+	T y = x[2] * (5.0 * x[0] + x[1]);
+
+	return y;
+};
+
+int test1()
+{
+	// Instrumented code
+	// Initialize tape first
+	Number::tape->rewind();
+
+	// Initialize inputs, including data members
+	// Note that puts inputs on tape, as leave nodes
+	Multiplier<Number> doubler(2.0);
+	Number y(5.0);
+	// The calculation itself, in this case, a simple product
+	Number z = doubler(y);
+	// Adjoint propagation
+	z.propagateToStart();
+	// Display results
+	cout << z.value() << endl; // 10.0
+	cout << y.adjoint() << endl; // 2.0
+	cout << doubler.multiplyBy.adjoint() << endl; // 5.0
+
+	return 0;
+}
+
+int test2()
+{
+	Number::tape->rewind();
+	Number x[5] = { Number(1.0), Number(2.0), Number(3.0), Number(4.0), Number(5.0) };
+	Number yy = f2(x);
+
+	yy.propagateToStart();
+
+
+
+	for (size_t i = 0; i < 5; ++i)
+	{
+		cout << "a" << i << " = " << x[i].adjoint() << endl;
+	}
+
+	return 0;
+
+
+}
+
+int test3()
+{
+
+	Number::tape->rewind();
+
+	Number spot(1.0);
+	Number rate(0.0);
+	Number yield(0.0);
+	Number vol(0.2);
+	Number strike(1.0);
+	Number mat(1.0);
+	Number price;
+	price = blackScholes(spot, rate, yield, vol, strike, mat);
+
+	price.propagateToStart();
+
+	cout << "price " << price.value() << endl;
+	cout << "spot " << spot.value() << " spot_der " << spot.adjoint() << endl;
+	cout << "rate " << rate.value() << " rate_der " << rate.adjoint() << endl;
+	cout << "yield " << yield.value() << " yield_der " << yield.adjoint() << endl;
+	cout << "vol " << vol.value() << " vol_der " << vol.adjoint() << endl;
+	cout << "strike " << strike.value() << " strike_der " << strike.adjoint() << endl;
+	cout << "mat " << mat.value() << " mat_der " << mat.adjoint() << endl;
+
+	return 0;
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	
+	test2();
+
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
